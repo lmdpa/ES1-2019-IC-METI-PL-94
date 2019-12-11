eaf40abd5629c533f6abd77d8560ebd44a536486
Andre Lopes <andreplopes99@hotmail.com>
2019-12-11 00:55:19 +0000

    regras

--- ProjetoES/src/GUI.java
+++ ProjetoES/src/GUI.java
@@ -61,13 +61,13 @@ public class GUI {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 593, 456);
+		frame.setBounds(100, 100, 777, 531);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JButton show_excel = new JButton("Show Excel");
-		show_excel.setBounds(222, 13, 101, 26);
+		show_excel.setBounds(294, 0, 138, 47);
 		show_excel.setMaximumSize(new Dimension(40, 25));
-		show_excel.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		show_excel.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		show_excel.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				File excel = new File("Long-Method.xlsx");
@@ -87,125 +87,154 @@ public class GUI {
 		
 		
 		JComboBox cbox1 = new JComboBox();
-		cbox1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				//aqui é suposto ir buscar o valor que esta no excel
-				
-			}
-		});
+		cbox1.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		cbox1.setToolTipText("Metricas");
-		cbox1.setBounds(12, 102, 127, 22);
+		cbox1.setBounds(50, 156, 130, 39);
 		cbox1.setEditable(true);
-		cbox1.addItem("LOC");
-		cbox1.addItem("CYCLO");
-		//cbox1.addItem("ATFD");
-		//cbox1.addItem("LAA");
-		cbox1.setSelectedItem(null);
+		cbox1.setSelectedItem("metrica 1");
 		frame.getContentPane().add(cbox1);
 		
 		
-		
 		JComboBox cbox2 = new JComboBox();
-		cbox2.setBounds(206, 102, 127, 22);
-		cbox2.setBorder(UIManager.getBorder("ComboBox.border"));
+		cbox2.setBounds(345, 165, 41, 22);
 		cbox2.setEditable(true);
-		cbox2.addItem("<");
 		cbox2.addItem(">");
+		cbox2.addItem("<");
 		cbox2.setSelectedItem(null);
 		frame.getContentPane().add(cbox2);
 		
+		
+		JComboBox cbox3 = new JComboBox();
+		cbox3.setToolTipText("");
+		cbox3.setBounds(310, 247, 105, 33);
+		cbox3.setEditable(true);
+		cbox3.addItem("OR");
+		cbox3.addItem("AND");
+		cbox3.setSelectedItem(null);
+		frame.getContentPane().add(cbox3);
+		
+		JComboBox cbox4 = new JComboBox();
+		cbox4.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		cbox4.setToolTipText("");
+		cbox4.setBounds(50, 342, 130, 39);
+		cbox4.setEditable(true);
+		cbox4.setSelectedItem("metrica 2");
+		frame.getContentPane().add(cbox4);
+		
+		
+
+		
+		JComboBox cbox5 = new JComboBox();
+		cbox5.setBounds(345, 329, 41, 22);
+		cbox5.setEditable(true);
+		cbox5.addItem(">");
+		cbox5.addItem("<");
+		cbox5.setSelectedItem(null);
+		frame.getContentPane().add(cbox5);
+		
+		
+		textField = new JTextField();
+		textField.setBounds(557, 157, 130, 39);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(557, 343, 130, 39);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		
 		JRadioButton ife = new JRadioButton("is_feature_envy");
+		ife.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		JRadioButton ilm = new JRadioButton("is_long_method");
+		ilm.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		ilm.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if(ilm.isSelected())
 					ife.setSelected(false);
+					cbox1.removeItem("ATFD");
+					cbox4.removeItem("LAA");
+					cbox1.addItem("LOC");
+					cbox4.addItem("CYCLO");
 			}
 		});
-		ilm.setBounds(369, 50, 127, 25);
+		ilm.setBounds(50, 32, 168, 56);
 		frame.getContentPane().add(ilm);
 		
 		ife.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				if(ife.isSelected())
 					ilm.setSelected(false);
+					cbox1.removeItem("LOC");
+					cbox4.removeItem("CYCLO");
+					cbox1.addItem("ATFD");
+					cbox4.addItem("LAA");
 			}
 		});
-		ife.setBounds(44, 41, 127, 25);
+		ife.setBounds(50, 77, 154, 58);
 		frame.getContentPane().add(ife);
 		
-		JComboBox cbox3 = new JComboBox();
-		cbox3.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		cbox3.setToolTipText("");
-		cbox3.setBounds(12, 260, 127, 22);
-		cbox3.setEditable(true);
-		//cbox3.addItem("LOC");
-		//cbox3.addItem("CYCLO");
-		cbox3.addItem("ATFD");
-		cbox3.addItem("LAA");
-		cbox3.setSelectedItem(null);
-		frame.getContentPane().add(cbox3);
 		
 		
-		JComboBox cbox4 = new JComboBox();
-		cbox4.setToolTipText("");
-		cbox4.setBounds(206, 260, 127, 22);
-		cbox4.setEditable(true);
-		cbox4.addItem("<");
-		cbox4.addItem(">");
-		cbox4.setSelectedItem(null);
-		frame.getContentPane().add(cbox4);
 		
 		
-		JComboBox cbox5 = new JComboBox();
-		cbox5.setToolTipText("");
-		cbox5.setBounds(253, 184, 46, 22);
-		cbox5.setEditable(true);
-		cbox5.addItem("OR");
-		cbox5.addItem("AND");
-		cbox5.setSelectedItem(null);
-		frame.getContentPane().add(cbox5);
 		
+		JButton criar_regra = new JButton("criar regra");
+		criar_regra.setBounds(294, 416, 138, 47);
+		criar_regra.setBorder(UIManager.getBorder("Button.border"));
+		criar_regra.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		criar_regra.setPreferredSize(new Dimension(40, 25));
+		criar_regra.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		
-		textField = new JTextField();
-		textField.setBounds(413, 102, 116, 22);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(413, 260, 116, 22);
-		frame.getContentPane().add(textField_1);
-		textField_1.setColumns(10);
-		
-		
-		JButton detectar_defeitos = new JButton("detectar defeitos");
-		detectar_defeitos.setBounds(219, 371, 144, 25);
-		detectar_defeitos.setBorder(UIManager.getBorder("Button.border"));
-		detectar_defeitos.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
-		detectar_defeitos.setPreferredSize(new Dimension(40, 25));
-		detectar_defeitos.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		detectar_defeitos.addActionListener(new ActionListener() {
+		criar_regra.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				LerExcel le = new LerExcel();
-				if (ilm.isSelected()) {
-					ife.setSelected(false);
-					int v1 = Integer.parseInt(textField.getText());	
-					int v2 = Integer.parseInt(textField_1.getText());
-					System.out.println("is_long_method is " +le.ilm_check(140, 30, v1, v2));
+				String metrica1 = (String)cbox1.getSelectedItem();
+				String comparador1 = (String)cbox2.getSelectedItem();
+				String and_or = (String)cbox3.getSelectedItem();
+				String metrica2 = (String)cbox4.getSelectedItem();
+				String comparador2 = (String)cbox5.getSelectedItem();
+				int v1 = Integer.parseInt(textField.getText());	
+//				int loc;
+//				int cyclo;
+//				int atfd;
+//				double laa;
+				
+				if(metrica1.equals("LOC") && comparador1.equals(">") && and_or.equals("AND") && metrica2.equals("CYCLO") && comparador2.equals(">")){
+					System.out.println(" Regra para testar is_long_method criada");
+					int v2 = Integer.parseInt(textField_1.getText());	
+					System.out.println("threshold da métrica 1 : " + v1);
+					System.out.println("threshold da métrica 2 : " + v2);
+					//le.ilm_regra1(loc, cyclo, v1, v2);
 				}
-				else if(ife.isSelected()) {
-					ilm.setSelected(false);
-					int v1 = Integer.parseInt(textField.getText());	
-					double v2 = Double.parseDouble(textField_1.getText());
-					System.out.println("is_feature_envy is " +le.ife_check(10, 0.2, v1, v2));
+				else if(metrica1.equals("LOC") && comparador1.equals(">") && and_or.equals("OR") && metrica2.equals("CYCLO") && comparador2.equals(">")){
+					System.out.println("Regra para testar is_long_method criada");
+					int v2 = Integer.parseInt(textField_1.getText());	
+					System.out.println("threshold da métrica 1 : " + v1);
+					System.out.println("threshold da métrica 2 : " + v2);
+					//le.ilm_regra2(loc, cyclo, v1, v2);
+					
+				}
+				
+				else if(metrica1.equals("ATFD") && comparador1.equals(">") && and_or.equals("AND") && metrica2.equals("LAA") && comparador2.equals("<")){
+					System.out.println("Regra para testar is_feature_envy criada");
+					Double vd2= Double.parseDouble(textField_1.getText());
+					System.out.println("threshold da métrica 1 : " + v1);
+					System.out.println("threshold da métrica 2 : " + vd2);
+					//le.ife_regra1(atfd, laa, v1, vd2);
+					
+				}
+				else if(metrica1.equals("ATFD") && comparador1.equals(">") && and_or.equals("OR") && metrica2.equals("LAA") && comparador2.equals("<")){
+					System.out.println("Regra para testar is_feature_envy criada");
+					Double vd2= Double.parseDouble(textField_1.getText());
+					System.out.println("threshold da métrica 1 : " + v1);
+					System.out.println("threshold da métrica 2 : " + vd2);
+					//le.ife_regra2(atfd, laa, v1, vd2);
+			
 				}
 			}
 		});
-		frame.getContentPane().add(detectar_defeitos);
-		
+		frame.getContentPane().add(criar_regra);
 		
 		
 		
--- ProjetoES/src/LerExcel.java
+++ ProjetoES/src/LerExcel.java
@@ -44,24 +44,41 @@ public class LerExcel {
 	}
 	
 	
-	// tentativa de regra do is long
-	public boolean ilm_check(int LOC, int CYCLO,int max_LOC, int max_CYCLO) {
+	// Regras................
+	//is long method
+	
+	public boolean ilm_regra1(int LOC, int CYCLO,int max_LOC, int max_CYCLO) {
 		boolean is_long_method;
-		if( LOC >= max_LOC || CYCLO >= max_CYCLO ||  LOC >= max_LOC && CYCLO >= max_CYCLO )
+		if(  LOC >= max_LOC && CYCLO >= max_CYCLO )
 			is_long_method = true;
-		else
-			is_long_method = false;
+		else is_long_method = false;
 	return is_long_method;
 	}
 	
 	
-	//Esta regra ta fadjoca ( is feature
-	public boolean ife_check(int ATFD, double LAA,double max_ATFD, double max_LAA) {
+	public boolean ilm_regra2(int LOC, int CYCLO,int max_LOC, int max_CYCLO) {
+		boolean is_long_method;
+		if( LOC >= max_LOC || CYCLO >= max_CYCLO)
+			is_long_method = true;
+		else is_long_method = false;
+	return is_long_method;
+	}
+	
+	// is feature envy
+	
+	public boolean ife_regra1(int ATFD, double LAA,double max_ATFD, double max_LAA) {
 		boolean is_feature_envy;
-		if( ATFD >= max_ATFD || LAA < max_LAA ||  ATFD > max_ATFD && LAA < max_LAA )
+		if( ATFD >= max_ATFD && LAA < max_LAA )
 			is_feature_envy = true;
-		else
-			is_feature_envy = false;
+		else is_feature_envy = false;
+	return is_feature_envy;
+	}
+	
+	public boolean ife_regra2(int ATFD, double LAA,double max_ATFD, double max_LAA) {
+		boolean is_feature_envy;
+		if( ATFD > max_ATFD || LAA < max_LAA )
+		is_feature_envy = true;
+		else is_feature_envy = false;
 	return is_feature_envy;
 	}
 	
